18cf59412e74bfc7353635d6723ff4d1ba179649
ssosag <santiago.sg003@gmail.com>
2022-10-16 04:15:52 -0500

    package futbol

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,34 @@
+package futbol;
+
+public class Jugador extends Futbolista {
+	public short golesMarcados;
+	public byte dorsal;
+	
+	public Jugador(String nombre, int edad, String posicion, short goles, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = goles;
+		this.dorsal = dorsal;
+	}
+	
+	public Jugador() {
+		super();
+		golesMarcados = 289;
+		dorsal = 7;
+	}
+
+	@Override
+	public int compareTo(Futbolista o) {
+		return Math.abs(o.getEdad() - this.getEdad());
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " con el dorsal" + dorsal + ".Ha marcado " + golesMarcados;
+	}
+	
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,29 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	public short golesRecibidos;
+	public byte dorsal;
+
+	public Portero(String nombre, int edad, short goles, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos = goles;
+		this.dorsal = dorsal;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " con el dorsal" + dorsal + ".Le han marcado" + golesRecibidos;
+	}
+
+	@Override
+	public int compareTo(Futbolista o) {
+		Portero p = (Portero) o;
+		return Math.abs(p.golesRecibidos - this.golesRecibidos);
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	
+}
